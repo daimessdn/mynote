2c0293086b5cc29621ef7a342ab2263480691259
Dimas <daimessdn@gmail.com>
2019-06-26 13:24:00 +0700

    created cards object...

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import Card from './components/Card'
 import './App.css';
 
 function App() {
+  const cards = [
+    {
+      id   : 433,
+      title: 'Hello',
+      desc : 'What\'s your name'
+    },
+    {
+      id   : 434,
+      title: 'Hi',
+      desc : 'Nice to meet you'
+    },
+    {
+      id   : 435,
+      title: 'Lorem ipsum',
+      desc : '... dolor sit amet ...'
+    },
+  ]
+
   return (
     <div className="card-group">
       <Card title="Hello" desc="What's your name" />
