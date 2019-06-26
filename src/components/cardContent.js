79a2d27efd689c441814b960b6afb23203552567
daimessdn@gmail.com <daimessdn@gmail.com>
2019-06-26 20:07:52 +0700

    set objects for mapping...

---
+++ src/components/cardContent.js
@@ -0,0 +1,17 @@
+const cardContent = [
+  {
+    id   : 433,
+    title: 'Hello',
+    desc : 'What\'s your name'
+  },
+  {
+    id   : 434,
+    title: 'Hi',
+    desc : 'Nice to meet you'
+  },
+  {
+    id   : 435,
+    title: 'Lorem ipsum',
+    desc : '... dolor sit amet ...'
+  },
+]
