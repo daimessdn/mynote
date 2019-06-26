b27cc94a5eeecb7062aaddd251fb08704196be4e
daimessdn@gmail.com <daimessdn@gmail.com>
2019-06-26 20:10:45 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import logo from './logo.svg';
 import Card from './components/Card'
 import './App.css';
 
-import cardContent from './components/cardContent'
-
-
-
 function App() {
- 
-  const content = cardContent.map(card => <Card key={card.id} title={card.title} desc={card.desc} />)
-
   return (
     <div className="card-group">
-      { content }
       <Card title="Hello" desc="What's your name" />
       <Card title="Hi" desc="Nice to meet you" />
       <Card title="Lorem ipsum" desc="... dolor sit amet ..." />
