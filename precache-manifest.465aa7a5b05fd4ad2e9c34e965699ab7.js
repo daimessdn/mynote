822f8f13459e051069da73e2b94b4d6e3eacb760
Dimas W <daimessdn@gmail.com>
2020-11-26 02:47:42 +0700

    Updates

---
+++ precache-manifest.465aa7a5b05fd4ad2e9c34e965699ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f267332b90007ec8a16580c746182752",
+    "url": "/mynote/index.html"
+  },
+  {
+    "revision": "2c994c76d34660f5e876",
+    "url": "/mynote/static/css/main.74fbe63e.chunk.css"
+  },
+  {
+    "revision": "2fb237b43f5351f7ba2f",
+    "url": "/mynote/static/js/2.1981694e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mynote/static/js/2.1981694e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c994c76d34660f5e876",
+    "url": "/mynote/static/js/main.a3262d15.chunk.js"
+  },
+  {
+    "revision": "f334d662de5dd0d20be4",
+    "url": "/mynote/static/js/runtime-main.bc3ee0b2.js"
+  }
+]);
